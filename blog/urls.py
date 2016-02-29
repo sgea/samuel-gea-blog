27a231d6a6c1e593646a3796ea864c0c252c677d
Samuel Gea <sgea111@gmail.com>
2016-02-29 20:05:41 -0300

    Adicionado css, adicionado funcao de editar post e de criar posts por form, como também separacao do header e do footer em outros arquivo

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url
 from .views import(
 	home,
 	posts,
+	post_editar,
+	post_create,
+	post_detalhe,
 	sobre,
 	portfolio
 )
 
 urlpatterns = [
-	url(r'^$', home),
-	url(r'^posts/$', posts),
-	url(r'^sobre/$', sobre),
-	url(r'^portfolio/$', portfolio)
+	url(r'^$', home, name='home' ),
+	url(r'^posts/$', posts, name='posts' ),
+	url(r'^posts/create/$', post_create, name='post_create' ),
+	url(r'^posts/(?P<id>[0-9]+)/$', post_detalhe, name='post_detalhe' ),
+	url(r'^posts/(?P<id>[0-9]+)/editar/$', post_editar, name='post_editar' ),
+	url(r'^sobre/$', sobre, name='sobre' ),
+	url(r'^portfolio/$', portfolio, name='portfolio' )
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post, Portfolio
+from .forms import PostForm
 
 # Create your views here.
 def home( request ):
@@ -8,14 +9,50 @@ def home( request ):
 	return render( request, 'home.html', context )
 
 def posts( request ):
-	posts = Post.objects.filter().order_by('criado')
-	context = { 'title' : 'Postagens', 'posts' : posts }
+	posts = Post.objects.order_by('-criado')
+	context = { 'title' : 'Posts', 'posts' : posts }
 	return render( request, 'posts.html', context )
+
+def post_create( request ):
+	if request.method == 'POST':
+		form = PostForm( request.POST )
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.criado = timezone.now()
+			post.save()
+			return redirect('posts')
+	else:
+		form = PostForm()
+	context = { 'title' : 'Criar Post', 'form' : form }
+	return render( request, 'post_create.html', context )
 
 def sobre( request ):
 	context = { 'title' : 'Sobre' }
 	return render( request, 'sobre.html', context )
 
 def portfolio( request ):
-	context = { 'title' : 'portfolio' }
+	context = { 'title' : 'Portfolio' }
 	return render( request, 'portfolio.html', context )
+
+def post_detalhe( request, id ):
+	post = get_object_or_404( Post, id=id)
+	context = { 'title' : post.titulo, 'post' : post }
+	return render( request , 'post_detalhe.html', context )
+
+def post_editar( request, id ):
+	post = get_object_or_404( Post, id=id )
+
+	if request.method == 'POST':
+		form = PostForm( request.POST, instance=post)
+		if form.is_valid():
+			post = form.save( commit=False )
+			post.autor = request.user
+			post.craido = timezone.now()
+			post.save()
+			return redirect('posts')
+	else:
+		form = PostForm( instance=post )
+
+	context = { 'title' : 'Editar Post', 'form' : form, 'post' : post }
+	return render( request, 'post_editar.html', context)
